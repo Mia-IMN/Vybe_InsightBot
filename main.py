b7c0e623e811c354e1d7b2f8a6d7985362af41ee
Mia-IMN <mira.ikechukwu15@gmail.com>
2025-05-01 18:51:47 +0100

    Fixed requirements.txt error

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ logger = logging.getLogger(__name__)
 MAIN_MENU, ASSET_SELECTION, TIMEFRAME_SELECTION, METRIC_SELECTION = range(4)
 
 # Configuration
-BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "Insert Telegram Bot Token here") # Get this from the requirements.txt file
-VYBE_API_KEY = os.environ.get("VYBE_API_KEY", "Insert Vybe API Key here") # Get this from the requirements.txt file
+BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8125089797:AAHKDU3TzaeK74Vsl-KIV60UD-ddz-N-Vic")
+VYBE_API_KEY = os.environ.get("VYBE_API_KEY", "ZYofNVCsyqxkcTDNNvo9MhCSyFBmNe35QMLTcts9BtThuCFE")
 VYBE_API_BASE_URL = "https://api.vybenetwork.xyz"
 ALPHAVYBE_BASE_URL = "https://alphavybe.fyi"
 
